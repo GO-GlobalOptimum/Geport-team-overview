e1657c8d3d61fe43863982b577f6d3e6e895089b
whitekuj02 <whitekuj02@naver.com>
2024-03-16 14:46:41 +0900

    일단 커밋..?

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import ToDo from './ToDo';
+import React, {useState} from "react";
+import {Container, List, Paper} from "@mui/material";
+import AddTodo from './AddTodo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    const [items, setItems] = useState([{
+        id : "0",
+        title : "Hello world 1",
+        done : true
+    },{
+        id : "1",
+        title : "Hello world 2",
+        done : true
+    }]) // 전체 item 리스트
+
+    const editItem = () => {
+        setItems([...items])
+    } // 고친 item을 setItem으로 전체 아이템 reloading
+
+    const deleteItem = (item) => {
+        // 삭제하고 싶은 item을 제외한 모든 item list
+        const newItems = items.filter(e => e.id !== item.id);
+        // 다시 전체 item list를 reloading
+        setItems([...newItems]);
+    }
+
+    // item의 길이가 1 이상
+    // 각 item의 ToDo page를 만들어줌
+    let todoItems = items.length > 0 &&
+        (
+            <Paper style={{margin:16}}>
+                <List>
+                    {items.map((item)=>
+                        <ToDo item={item} key={item.id}
+                              editItem={editItem}
+                              deleteItem={deleteItem}/>
+                        // 각 item component에 id로 key
+                    )}
+                </List>
+            </Paper>
+        )
+
+    const addItem = (item) => {
+        item.id = "ID-" + items.length;
+        item.done = false;
+
+        setItems([...items, item])
+        console.log("items: ", items);
+    }
+    return (<div className="App">
+        <Container maxWidth="md">
+            <AddTodo addItem={addItem} />
+            <div className="TodoList">{todoItems}</div>
+        </Container>
+    </div>);
+
 }
-
 export default App;
